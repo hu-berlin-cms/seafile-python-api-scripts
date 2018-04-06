088bb411f8b4a82bbe25ab63aa9d5c0c64060fda
Enno Gröper <groepeen@cms.hu-berlin.de>
2018-04-06 15:08:12 +0200

    view_history.py: Refactoring for being reusable
    Be able to produce flexible output when traversing commit tree.

--- view_history.py
+++ view_history.py
@@ -8,18 +8,28 @@ import sys
 
 traversed_commits = set()
 
-def traverse_commits(commit_mgr, repo_id, repo_version, commit_id):
+
+def print_commit_edge_list(commit):
+    if commit.parent_id:
+        print "%.8s,%.8s" % (commit.commit_id, commit.parent_id)
+    if commit.second_parent_id:
+        print "%.8s,%.8s" % (commit.commit_id, commit.second_parent_id)
+
+
+def traverse_commits(commit_mgr, func, repo_id, repo_version, commit_id,
+                     **kwargs):
     traversed_commits.add(commit_id)
     commit = commit_mgr.load_commit(repo_id, repo_version, commit_id)  # noqa
+    func(commit)
     if commit.parent_id:
-        print "%.8s,%.8s" % (commit_id, commit.parent_id)
         if commit.parent_id not in traversed_commits:
-            traverse_commits(commit_mgr, repo_id, repo_version, commit.parent_id)
-    if commit.second_parent_id:
-        print "%.8s,%.8s" % (commit_id, commit.second_parent_id)
+            traverse_commits(commit_mgr, func, repo_id, repo_version,
+                             commit.parent_id, **kwargs)
+    if commit.second_parent_id and ('only_parent' not in kwargs or
+                                    not kwargs['only_parent']):
         if commit.second_parent_id not in traversed_commits:
-            traverse_commits(commit_mgr, repo_id, repo_version,
-                             commit.second_parent_id)
+            traverse_commits(commit_mgr, func, repo_id, repo_version,
+                             commit.second_parent_id, **kwargs)
 
 
 def main():
@@ -39,7 +49,10 @@ def main():
     if not repo:
         sys.exit(-1)
 
-    traverse_commits(commit_mgr, repo.id, repo.version, repo.head_cmmt_id)
+    if args.tree:
+        print "edge list:"
+        traverse_commits(commit_mgr, print_commit_edge_list, repo.id,
+                         repo.version, repo.head_cmmt_id)
 
 
 if __name__ == '__main__':
